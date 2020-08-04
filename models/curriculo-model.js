e2c8272321e7209e2e37c761e3dc31fb4823d73d
FEH-SAMA <felipe.tomaz.rebeca.br@gmail.com>
2020-08-04 16:04:47 -0400

    Subindo todos os Arquivos

---
+++ models/curriculo-model.js
@@ -0,0 +1,18 @@
+const data = {
+    title: 'Meu Curriculo',
+    name: 'Felipe Tomaz Rebeca',
+    profession: 'Software Engineer',
+    description: 'Expêriencia em desenvolvimento de single page applications com JavaScript e frameworks Javascript (Já trabalhei com Angular e VueJS), e NodeJS (Desenvolvimento de Bots para o Discord).',
+    experience: [{
+        company: 'Skay S0',
+        office: 'FrontEnd',
+        description: 'Trabalho na parte de desenvolvimento de Portfólios.'
+    }],
+    education: [{
+        institution: 'E. E. Antônio Andŕe Maggi',
+        description: 'Estudante do ensino Fundamental'
+    }],
+    skills: ['backend', 'frontend']
+}
+
+module.exports = data;
